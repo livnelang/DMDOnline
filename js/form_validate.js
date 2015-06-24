ca581ef574cb1314b6a13d91362f74f2f4827953
Livne Lang <livnelang@gmail.com>
2015-06-24 12:10:51 +0300

    Second Commit

--- js/form_validate.js
+++ js/form_validate.js
@@ -6,33 +6,44 @@ $(document).ready(function(){
 });
 
 
-
+/**
+* Validate Only Letters
+*/
 function isAlphaOrParen(str) {
   return /^[a-zA-Z()]+$/.test(str);
 }
 
+/**
+* Validate Mail Accuracy
+*/
 function IsEmail(email) {
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   return regex.test(email);
 }
 
- // Validate Form  
+/**
+* ValidateForm Function
+*/  
 function ValidateForm() {
-	// alert('hello');
 	var name,email,approved;
 	var my_form = document.forms[0];
 	name = my_form.elements["fullname"].value;
 	email = my_form.elements["mail"].value;
 	if ( isAlphaOrParen(name) ) {
-	//	alert	('true name');
 		if ( IsEmail(email) ) {
 			return true;
 		}
+
 		else {
 			window.alert('-- Please Check Your Form Details --');
 			console.log('-- Please Check Your Form Details --');
 			return false; 
 		}	
 	}
-	
+
+		else {
+			window.alert('-- Please Check Your Form Details --');
+			console.log('-- Please Check Your Form Details --');
+			return false; 
+		}
 }
